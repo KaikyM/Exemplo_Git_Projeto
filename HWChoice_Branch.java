7fed381b4bf47bcc105303602a8a9babb3c38704
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-09-24 22:51:40 -0300

    Update HWChoice_Branch.java

--- HWChoice_Branch.java
+++ HWChoice_Branch.java
@@ -2,14 +2,14 @@ import javax.swing.JOptionPane;
 public class Main{
 	public static void main(String[] args){
 		String choice = JOptionPane.showInputDialog(null, "Você quer que apareca Hello World na sua tela? (S/N)");
-        char choiceChar= choice.toUpperCase().charAt(0);
-        switch (choiceChar) {
-            case 'S':
-                JOptionPane.showMessageDialog(null,"Hello World");
-                break;
-            case 'N':
-                JOptionPane.showMessageDialog(null,"Volte Sempre");
-                break;
-        }
+        	char choiceChar= choice.toUpperCase().charAt(0);
+        	switch (choiceChar) {
+            		case 'S':
+                		JOptionPane.showMessageDialog(null,"Hello World");
+                	break;
+            		case 'N':
+                		JOptionPane.showMessageDialog(null,"Volte Sempre");
+                	break;
+        	}
 	}
 }
